5c23213dec398eac2ffd6ebf828e52cf7332139e
Betters <591961@bah.com>
2017-11-22 14:43:53 -0500

    Disabled host key checking in ansible.cfg and added a batch script to run the site provisioning

---
+++ provision_site.bat
@@ -0,0 +1,1 @@
+ansible-playbook /cygdrive/c/Users/591961/Desktop/ozp/env/vms/ozp-ansible/site.yml -i /cygdrive/c/Users/591961/Desktop/ozp/env/vms/ozp-ansible/hosts_vagrant -u vagrant -k --ask-become-pass
